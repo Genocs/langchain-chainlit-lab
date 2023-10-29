5ed6c8cf31e83afa7272da4cdb2a4e91561607da
Nocco Giovanni Emanuele <giovanni.nocco@gmail.com>
2023-10-29 12:19:30 +0100

    added QA with Croma Vector database

--- src/main.py
+++ src/main.py
@@ -2,10 +2,17 @@
 import sys
 import os
 
-from langchain import OpenAI
-from langchain.agents import create_csv_agent
-from chainlit import user_session
+from langchain.llms import OpenAI
 import chainlit as cl
+from chainlit import user_session
+
+from langchain_experimental.agents.agent_toolkits.csv.base import create_csv_agent
+
+from langchain.vectorstores import Chroma
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains import RetrievalQA
+from langchain.document_loaders import TextLoader
 
 sys.path.append(os.path.abspath('.'))
 
@@ -26,7 +33,18 @@ def factory():
     # Instantiate the chain for that user session
     # prompt = PromptTemplate(template=TEMPLATE, input_variables=["question"])
 
-    llm = OpenAI(temperature=0, model_name=MODEL_NAME)
-    csv_agent = create_csv_agent(llm=llm, path='/home/appuser/app/src/titanic.csv', verbose=True)
+    # llm = OpenAI(temperature=0, model_name=MODEL_NAME)
+    # csv_agent = create_csv_agent(llm=llm, path='src/titanic.csv', verbose=True)
 
-    return csv_agent
+    loader = TextLoader('src/company_description.txt')
+    documents = loader.load()
+
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    texts = text_splitter.split_documents(documents)
+
+    embeddings = OpenAIEmbeddings()
+    vectordb = Chroma.from_documents(texts, embeddings)
+
+    qa = RetrievalQA.from_chain_type(llm=OpenAI(), chain_type="stuff", retriever=vectordb.as_retriever())
+
+    return qa
